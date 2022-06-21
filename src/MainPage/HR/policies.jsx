b1e613bb5553d7cd2210a0dd9658fc53cb37e525
maham-Orange-lynx <96826032+maham-Orange-lynx@users.noreply.github.com>
2022-06-22 03:24:20 +0500

    policies crud

--- src/MainPage/HR/policies.jsx
+++ src/MainPage/HR/policies.jsx
@@ -1,27 +1,140 @@
 
-import React, { useEffect,useState } from 'react';
+import React, { useEffect,useState, useRef } from 'react';
 import { Helmet } from "react-helmet";
 import { Link } from 'react-router-dom';
-
+import axios from "axios";
 import { Table } from 'antd';
 import 'antd/dist/antd.css';
 import {itemRender,onShowSizeChange} from "../paginationfunction"
 import "../antdstyle.css"
+import jwt from "../../auth/useJwt";
+import $ from "jquery";
+import SimpleReactValidator from "simple-react-validator";
+import Select from 'react-select';
+import { Spinner } from 'reactstrap';
 
 const Policies = () => {
-  const [data, setData] = useState([
-    {id:1,policyname:"John Doe",description:"Lorem ipsum dollar",department:"IOS",creatat:"1 Jan 2013",status:"Active"},
-         {id:2,policyname:"Richard Miles",description:"Lorem ipsum dollar",department:"Design",creatat:"18 Mar 2014",status:"Active"},
-  ]);
-  useEffect( ()=>{
-    if($('.select').length > 0) {
-      $('.select').select2({
-        minimumResultsForSearch: -1,
-        width: '100%'
+  const [data, setData] = useState([]);
+  const [IsLoading, setIsLoading] = useState(true);
+  const [selectedDepartment, setSelectedDepartment] = useState('4');
+  const simpleValidator = useRef(new SimpleReactValidator());
+  const [, forceUpdate] = useState();
+  const form = React.createRef();
+  const initialFormData = Object.freeze({});
+  const [departments, setDepartments] = useState([{label:'All Departments',value:'4'},{label:'Web Development',value:'1'},{label:'Marketing',value:'2'},{label:'IT Managment',value:'3'}])
+  const [formData, updateFormData] = useState({
+    name: "",
+    description:"",
+    department_id:"",
+    document:"",
+  });
+  
+  const [formDataValidation, updateFormDataValidation] = useState({
+    name: "",
+    description:"",
+    department_id:"",
+   
+  });
+  const handleChange = (e) => {
+    if(e.target.name == 'document'){
+      updateFormData({
+        ...formData,
+          
+        // Trimming any whitespace
+      [e.target.name]: e.target.files[0]
+      });
+    }else{
+      updateFormData({
+        ...formData,
+          
+        // Trimming any whitespace
+        [e.target.name]: e.target.value.trim()
       });
     }
-  });  
+    
+  };
   
+  
+  useEffect( ()=>{
+    getPolicies()
+    // if($('.select').length > 0) {
+    //   $('.select').select2({
+    //     minimumResultsForSearch: -1,
+    //     width: '100%'
+    //   });
+    // }
+  },[]);  
+  const getPolicies = (e) => {
+    jwt
+   .get('/policies')
+   .then((res) => {
+     setData(res.data);
+     setIsLoading(false)
+   })
+   .catch((err) =>{ console.log(err);
+     setIsLoading(false)
+     setError("general", {
+          message: err.response.data.message,
+        });
+   });
+  }
+  const download = (e) => {
+    console.log("clicked")
+    jwt.get('/download', {
+      headers: {
+        responseType :"blob"
+      }
+    }).then((res) => {
+        const url = window.URL.createObjectURL(new Blob([res.data]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'file.pdf'); //or any other extension
+        document.body.appendChild(link);
+        link.click();
+    }).catch((err) =>{ console.log(err);
+      console.log(err.errors)
+    
+    });
+    // axios({
+    //   url: 'http://127.0.0.1:8000/api/download',
+    //   method: 'GET',
+    //   responseType: 'blob', // important
+    // }).then((response) => {
+    //    const url = window.URL.createObjectURL(new Blob([response.data]));
+    //    const link = document.createElement('a');
+    //    link.href = url;
+    //    link.setAttribute('download', 'file.pdf'); //or any other extension
+    //    document.body.appendChild(link);
+    //    link.click();
+    // });
+  }
+  const handleSubmit = (e) => {
+      
+    const formValid = simpleValidator.current.allValid();
+    console.log(formValid)
+    if (!formValid) {
+      simpleValidator.current.showMessages(true);
+      forceUpdate(1)
+    } else {
+      formData.department_id = selectedDepartment
+        
+      jwt.post('/addPolicy',formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }).then((res) => {
+          if(res.success == true) {
+            $("#add_policy").modal("hide");
+            $(".modal-backdrop").hide();
+            getPolicies()
+          }
+      }).catch((err) =>{ console.log(err);
+        console.log(err.errors)
+      
+      });
+      console.log(formData);
+    }
+  };
     const columns = [
       
       {
@@ -31,13 +144,13 @@ const Policies = () => {
       }, 
       {
         title: 'Policy Name',
-        dataIndex: 'policyname', 
-        sorter: (a, b) => a.policyname.length - b.policyname.length,
+        dataIndex: 'name', 
+        sorter: (a, b) => a.name.length - b.name.length,
       }, 
       {
         title: 'Department',
-        dataIndex: 'department', 
-        sorter: (a, b) => a.department.length - b.department.length,
+        dataIndex: 'department_name', 
+        sorter: (a, b) => a.department_name.length - b.department_name.length,
       },        
       {
         title: 'Description',
@@ -46,8 +159,8 @@ const Policies = () => {
       },
       {
         title: 'Created',
-        dataIndex: 'creatat',
-        sorter: (a, b) => a.creatat.length - b.creatat.length,
+        dataIndex: 'created_date',
+        sorter: (a, b) => a.created_date.length - b.created_date.length,
       },
       {
         title: 'Action',
@@ -55,7 +168,7 @@ const Policies = () => {
             <div className="dropdown dropdown-action text-end">
                <a href="#" className="action-icon dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"><i className="material-icons">more_vert</i></a>
                 <div className="dropdown-menu dropdown-menu-right">
-                  <a className="dropdown-item" href="#"><i className="fa fa-download m-r-5" /> Download</a>
+                  <a className="dropdown-item" onClick={download}><i className="fa fa-download m-r-5" /> Download</a>
                   <a className="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#edit_policy"><i className="fa fa-pencil m-r-5" /> Edit</a>
                   <a className="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#delete_policy"><i className="fa fa-trash-o m-r-5" /> Delete</a>
                 </div>
@@ -66,7 +179,7 @@ const Policies = () => {
       return ( 
       <div className="page-wrapper">
         <Helmet>
-            <title>Policies - HRMS Admin Template</title>
+            <title>Policies - Riyo Admin Template</title>
             <meta name="description" content="Login page"/>					
         </Helmet>
         {/* Page Content */}
@@ -120,30 +233,43 @@ const Policies = () => {
                 <form>
                   <div className="form-group">
                     <label>Policy Name <span className="text-danger">*</span></label>
-                    <input className="form-control" type="text" />
+                    <input className="form-control" type="text" name="name"  onChange={handleChange}
+                              value={formData.name}
+                              onBlur={() => {
+                                simpleValidator.current.showMessageFor("name")
+                                forceUpdate(1);
+                              }} />
+                    {simpleValidator.current.message("name", formData.name, "required")}
                   </div>
+                  
                   <div className="form-group">
                     <label>Description <span className="text-danger">*</span></label>
-                    <textarea className="form-control" rows={4} defaultValue={""} />
+                    <textarea className="form-control" rows={4} defaultValue={""} name="description" onChange={handleChange}
+                              value={formData.description}
+                              onBlur={() => {
+                                simpleValidator.current.showMessageFor("description")
+                                forceUpdate(1);
+                              }}/>
+                    {simpleValidator.current.message("description", formData.description, "required")}
                   </div>
+                  
                   <div className="form-group">
-                    <label className="col-form-label">Department</label>
-                    <select className="select">
-                      <option>All Departments</option>
-                      <option>Web Development</option>
-                      <option>Marketing</option>
-                      <option>IT Management</option>
-                    </select>
+                    <label className="col-form-label">Department <span className="text-danger">*</span></label>
+                    <Select name="department_id" id="select-1" className="select" onChange={(e) => setSelectedDepartment(e.value)} options={departments} value={departments.filter(function(option) {
+                                  return option.value === selectedDepartment;
+                                })}>
+                    </Select>
                   </div>
                   <div className="form-group">
                     <label>Upload Policy <span className="text-danger">*</span></label>
                     <div className="custom-file">
-                      <input type="file" className="custom-file-input" id="policy_upload" />
-                      <label className="custom-file-label" htmlFor="policy_upload">Choose file</label>
+                      <input type="file" className="custom-file-input" id="policy_upload" name="document" onChange={handleChange}/>
+                      {/* <label className="custom-file-label" htmlFor="policy_upload">Choose file</label> */}
                     </div>
+          
                   </div>
                   <div className="submit-section">
-                    <button className="btn btn-primary submit-btn">Submit</button>
+                    <button type="button" onClick={handleSubmit} className="btn btn-primary submit-btn">Submit</button>
                   </div>
                 </form>
               </div>
